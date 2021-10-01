11152ed97f14dc23381e991217d06f3180ad9644
rafaeldr <41450571+rafaeldr@users.noreply.github.com>
2021-10-01 17:31:23 -0300

    Primeiros passos

--- PythonETL_PI/PythonETL_PI.py
+++ PythonETL_PI/PythonETL_PI.py
@@ -1,1 +1,8 @@
-print('teste')
+import xmltodict
+import pandas as pd
+import numpy as np
+
+df=pd.read_csv('..\DataSources\data_anvisa.csv', sep=';')
+
+with open(r"..\DataSources\full database.xml", encoding='utf-8') as fd:
+    doc = xmltodict.parse(fd.read())
