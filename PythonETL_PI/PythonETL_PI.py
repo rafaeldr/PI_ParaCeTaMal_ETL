a1a14776389e7b2fd46aec3ceedb1d852363751e
rafaeldr <41450571+rafaeldr@users.noreply.github.com>
2021-10-03 00:40:01 -0300

    Extraindo lista simples de princípios ativos da ANVISA

--- PythonETL_PI/PythonETL_PI.py
+++ PythonETL_PI/PythonETL_PI.py
@@ -5,10 +5,13 @@ import collections
 
 # File locations
 anvisa_file = r"..\DataSources\data_anvisa.csv"
-drugbank_file = r"..\DataSources\full database.xml"    # Full DrugBank - Takes ~6 min to parse
-#drugbank_file = r"..\DataSources\drugbank_sample6.xml"  # Sample DrugBank with only 6 drugs
+#drugbank_file = r"..\DataSources\full database.xml"    # Full DrugBank - Takes ~6 min to parse
+drugbank_file = r"..\DataSources\drugbank_sample6.xml"  # Sample DrugBank with only 6 drugs
+# Export
 exp_csv_drugs = r"..\DataSources\exp_csv_drugs.csv"
 exp_csv_interactions = r"..\DataSources\exp_csv_interactions.csv"
+exp_csv_pAtivos = r"..\DataSources\exp_csv_pAtivos.csv"
+
 
 # Importing Data Sources (AS-IS) - ANVISA
 df_anvisa = pd.read_csv(anvisa_file, sep=';')
@@ -54,3 +57,17 @@ df_interactions = pd.DataFrame(data)
 # Exporting
 df_drugs.to_csv(exp_csv_drugs, index = False)
 df_interactions.to_csv(exp_csv_interactions, index = False)
+
+# Anvisa - Extração Princípios Ativos
+s_pAtivos = df_anvisa['PRINCIPIO_ATIVO']  # Series
+listFullAtivos = list()
+for rowStr in s_pAtivos:
+    if type(rowStr)==str:
+        rowList = list(map(str.upper,list(map(str.strip, rowStr.split('+')))))
+        listFullAtivos.extend(rowList)
+    else:
+        continue
+
+s_pAtivosExp = pd.Series(list(set(listFullAtivos)))
+s_pAtivosExp.to_csv(exp_csv_pAtivos, encoding="utf-8", index = False)
+
