5f89253070f7d5b6f89fda06416c2fe6dd67018b
rafaeldr <41450571+rafaeldr@users.noreply.github.com>
2021-11-04 23:53:52 -0300

    Rascunho do módulo de exportação para SQL

--- PythonETL_PI/SQLModule.py
+++ PythonETL_PI/SQLModule.py
@@ -1,21 +1,21 @@
 import pandas as pd
 
 class SQLExport:
-    
-    def get_insert_query_from_df(df, dest_table):
 
-        insert = """
-        INSERT INTO `{dest_table}` (
-            """.format(dest_table=dest_table)
+def get_insert_query_from_df(df, dest_table):
 
-        columns_string = str(list(df.columns))[1:-1]
-        columns_string = re.sub(r' ', '\n        ', columns_string)
-        columns_string = re.sub(r'\'', '', columns_string)
+    insert = """
+    INSERT INTO `{dest_table}` (
+        """.format(dest_table=dest_table)
 
-        values_string = ''
+    columns_string = str(list(df.columns))[1:-1]
+    columns_string = re.sub(r' ', '\n        ', columns_string)
+    columns_string = re.sub(r'\'', '', columns_string)
 
-        for row in df.itertuples(index=False,name=None):
-            values_string += re.sub(r'nan', 'null', str(row))
-            values_string += ',\n'
+    values_string = ''
 
-        return insert + columns_string + ')\n     VALUES\n' + values_string[:-2] + ';'
+    for row in df.itertuples(index=False,name=None):
+        values_string += re.sub(r'nan', 'null', str(row))
+        values_string += ',\n'
+
+    return insert + columns_string + ')\n     VALUES\n' + values_string[:-2] + ';'
